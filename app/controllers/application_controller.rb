4de8049c24d954605f331839ed8927b5a33c61c7
Danila Babanov <babanovs5@gmail.com>
2015-07-07 19:04:46 +0400

    avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_devise_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_devise_parameters
     devise_parameter_sanitizer.for(:account_update) << :name
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,
+                                                            :password_confirmation, :remember_me, :avatar, :avatar_cache) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password,
+                                                                   :password_confirmation, :current_password, :avatar, :avatar_cache) }
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
+
   has_many :posts
   belongs_to :role
+
   validates :name, presence: true
+  validate  :avatar_size
+
+  private
+  def avatar_size
+    if avatar.size > 5.megabytes
+      errors.add(:user_avatar, "should be less than 5MB")
+    end
+  end
 
 end
