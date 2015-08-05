d9c7eeb5c87aed7966a1099d17b6d667daa30e9b
Danila Babanov <babanovs5@gmail.com>
2015-08-05 17:54:12 +0400

    emailmy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,21 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  config.cache_classes = true
-  config.eager_load = true
-
-  config.action_mailer.default_url_options = { :host => 'railsblog.pw' }
+  config.action_mailer.default_url_options = {:host => 'railsblog.pw'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'railsblog.pw',
-      user_name: 'babanovs5@gmail.com',
-      password: '0000099999qw',
-      authentication: 'plain',
-      enable_starttls_auto: true }
+      :address => "188.166.79.13",
+      :port    => 25,
+      :domain  => 'railsblog.pw'
+  }
+
 
 end
