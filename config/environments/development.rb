d3812424cd8e261e94ca0e658a760a6216cf86f1
Danila Babanov <babanovs5@gmail.com>
2015-08-05 18:03:15 +0400

    gmailemail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,27 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.perform_deliveries = true
+
+  config.cache_classes = false
+  config.eager_load = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+  config.action_mailer.default :charset => "utf-8"
+  config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'babanovs5@gmail.com',
+      password:             '0000099999qw',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,6 +17,18 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
+  config.mailer_sender = 'babanovs5@gmail.com'
+  config.mailer = 'Devise::Mailer'
+  require 'devise/orm/active_record'
+  config.case_insensitive_keys = [ :email ]
+  config.strip_whitespace_keys = [ :email ]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 10
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 8..128
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
